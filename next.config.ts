725291de384ea22c7130d456ad6f94379bdf5660
namqrc <134195368+namqrc@users.noreply.github.com>
2025-05-20 00:28:31 +0700

    3D second commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
